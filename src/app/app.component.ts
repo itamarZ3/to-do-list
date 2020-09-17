f58f61bebf55adef14018b79a0938fdb8f94c4e5
itamarZ3 <djitamarZ3@gmail.com>
2020-09-17 20:32:37 -0300

    Update

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  public tarefa = "";
+  public items = ['item 1', 'item 2', 'item 3', 'item 4',];
+
+  addTarefa(){
+    this.items.push(this.tarefa);
+  }
+
+  removeTarefa(item){
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+}
